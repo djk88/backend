2f762363e5c26763fa7366f8e5d2149b92a5c13e
djk88 <dkim104@my.bcit.ca>
2023-01-18 14:49:28 -0800

    changes to server.js

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config()
 
-// const { create } = require('domain')
+
 const express = require('express')
 const fs = require('fs')
 const multer = require('multer')
 const database = require('./database')
 const upload = multer({ dest: 'images/' })
-const path = require('path')
 const app = express()
 
-// app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static('dist'))
 
 app.get("/api/images", async (req, res) => {
@@ -17,28 +16,9 @@ app.get("/api/images", async (req, res) => {
     res.send(getImages)
 })
 
-// app.get('/api', (req, res) => {
-//     console.log('req.query', req.query)
-//     const imagePath = req.query.imagePath
-//     if (!imagePath) {
-//         res.send("")
-//         return
-//     }
-
-//     console.log('imagePath', imagePath)
-//     const readStream = fs.createReadStream(imagePath)
-//     readStream.pipe(res)
-// })
 
 app.get('/images/:imageName', (req, res) => {
-    //console.log('req.query', req.query)
-    //const imagePath = req.query.imagePath
-    //if (!imagePath) {
-    //    res.send("")
-    //    return
-    //}
 
-    //console.log('imagePath', imagePath)
     const imageName = req.params.imageName
     const readStream = fs.createReadStream(`images/${imageName}`)
     readStream.pipe(res)
@@ -50,14 +30,10 @@ app.post('/api/images', upload.single('image'), async (req, res) => {
     const createImages = await database.addImage(imagePath, description)
 
     console.log(description, imagePath)
-    // res.send({ description, imagePath })
+
     res.send(createImages)
 })
 
-// app.get('*', (_req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist/index.html'));
-// });
-
 const port = process.env.PORT || 8080
 
-app.listen(port, () => console.log("listening on port 8080"))
+app.listen(port, () => console.log("Server is running on port " + port))
