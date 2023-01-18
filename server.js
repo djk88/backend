21ef985dd4f1ada1fdca5846816c17dc1e1b057c
djk88 <dkim104@my.bcit.ca>
2023-01-18 14:27:37 -0800

    overhauled server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const upload = multer({ dest: 'images/' })
 const path = require('path')
 const app = express()
 
-app.use(express.urlencoded({ extended: true }))
+// app.use(express.urlencoded({ extended: true }))
 app.use(express.static('dist'))
 
 app.get("/api/images", async (req, res) => {
@@ -17,32 +17,46 @@ app.get("/api/images", async (req, res) => {
     res.send(getImages)
 })
 
-app.get('/api', (req, res) => {
-    console.log('req.query', req.query)
-    const imagePath = req.query.imagePath
-    if (!imagePath) {
-        res.send("")
-        return
-    }
+// app.get('/api', (req, res) => {
+//     console.log('req.query', req.query)
+//     const imagePath = req.query.imagePath
+//     if (!imagePath) {
+//         res.send("")
+//         return
+//     }
 
-    console.log('imagePath', imagePath)
-    const readStream = fs.createReadStream(imagePath)
+//     console.log('imagePath', imagePath)
+//     const readStream = fs.createReadStream(imagePath)
+//     readStream.pipe(res)
+// })
+
+app.get('/images/:imageName', (req, res) => {
+    //console.log('req.query', req.query)
+    //const imagePath = req.query.imagePath
+    //if (!imagePath) {
+    //    res.send("")
+    //    return
+    //}
+
+    //console.log('imagePath', imagePath)
+    const imageName = req.params.imageName
+    const readStream = fs.createReadStream(`images/${imageName}`)
     readStream.pipe(res)
 })
 
 app.post('/api/images', upload.single('image'), async (req, res) => {
-    const imagePath = req.file.path
+    const imagePath = 'images/' + req.file.filename
     const description = req.body.description
     const createImages = await database.addImage(imagePath, description)
 
     console.log(description, imagePath)
-    res.send({ description, imagePath })
+    // res.send({ description, imagePath })
     res.send(createImages)
 })
 
-app.get('*', (_req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+// app.get('*', (_req, res) => {
+//     res.sendFile(path.join(__dirname, 'dist/index.html'));
+// });
 
 const port = process.env.PORT || 8080
 
